934b5f7c13690b4ec3b0dad158f6cd1d7cf515ac
JackPu <kakashjack@gmail.com>
2017-02-04 17:41:54 +0800

    add web render

--- web/vue/action-sheet.vue
+++ web/vue/action-sheet.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="action-sheet-container">
+    <div class="as-dialog">
       
+    </div>    
   </div>
 </template>
 <style>
-  .action-sheet-container{
+  .as-container{
     position: fixed;
     left: 0;
     right: 0;
@@ -12,9 +14,18 @@
     bottom: 0;
     background-color: rgba(0,0,0,.5);  
   }
+  .as-dialog{
+    position: absolute;
+    left: 10;
+    right: 10;
+    bottom:10;
+    background-color: #fff;
+    border-radius: 10;
+    padding: 20;
+  }
 </style>
 <script>
   module.exports = {
-    
+      
   };
 </script>
