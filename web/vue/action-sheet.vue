c8cc33284305a52ff3bcb3324fa2004cee49a1c4
JackPu <kakashjack@gmail.com>
2017-02-04 19:46:17 +0800

    add new vue

--- web/vue/action-sheet.vue
+++ web/vue/action-sheet.vue
@@ -1,12 +1,25 @@
 <template>
-  <div class="action-sheet-container">
+  <div class="as-container">
     <div class="as-dialog">
-      
-    </div>    
+      <div class="as-hd">
+        <div class="as-title">{{title}}</div>
+        <div class="as-message">{{message}}</div>
+      </div>
+      <div class="as-btn">
+        <text class="as-btn-text">确认</text>
+      </div>
+      <div class="as-btn as-btn-remove">
+        <text class="as-btn-text as-text-remove">删除</text>
+      </div>
+      <div type="button" class="as-btn as-cancel">
+        <text class="as-btn-text">取消</text>
+      </div>
+    </div>      
   </div>
 </template>
 <style>
   .as-container{
+    flex:1;
     position: fixed;
     left: 0;
     right: 0;
@@ -16,16 +29,57 @@
   }
   .as-dialog{
     position: absolute;
-    left: 10;
-    right: 10;
-    bottom:10;
+    left: 10px;
+    right: 10px;
+    bottom: 10px;
+    padding: 20px;
+    
+  }
+  .as-hd{
+    padding: 20px 0;
     background-color: #fff;
-    border-radius: 10;
-    padding: 20;
+    align-items: center;
+    border-top-left-radius: 15px;
+    border-top-right-radius: 15px;
+  }
+  .as-title{
+    font-size: 26px;
+    margin-bottom: 15px;
+  }
+  .as-message{
+    font-size: 24px;
+  }
+  .as-btn{
+    font-size: 30px;
+    padding: 30px 0;
+    background-color: #fff;
+    border-top: 1px solid #ddd;
+    
+  }
+  .as-btn-text{
+    flex:1;
+    text-align: center;
+    color: #1ba1e2;
+  }
+  .as-btn-remove{
+    border-bottom-right-radius: 15px;
+    border-bottom-left-radius: 15px;
+  }
+  .as-text-remove{
+    color: red;
+  }
+  .as-cancel{
+    margin-top: 30px;
+    border-radius: 15px;
   }
 </style>
 <script>
   module.exports = {
-      
+    data() {
+      return {
+        message: 'your message',
+        title: 'title'
+      };
+    }    
   };
 </script>
