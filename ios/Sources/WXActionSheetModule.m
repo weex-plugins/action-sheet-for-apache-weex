41d5d1769c218f2dae353ffbcf4d7b32782e5865
齐山 <sunjjbobo@163.com>
2017-03-30 11:09:22 +0800

    update iOS

---
+++ ios/Sources/WXActionSheetModule.m
@@ -0,0 +1,77 @@
+/**
+ * Created by Weex.
+ * Copyright (c) 2016, Alibaba, Inc. All rights reserved.
+ *
+ * This source code is licensed under the Apache Licence 2.0.
+ * For the full copyright and license information,please view the LICENSE file in the root directory of this source tree.
+ */
+
+#import "WXActionSheetModule.h"
+#import <WeexSDK/WXConvert.h>
+#import <WeexPlugin/WeexPlugin.h>
+
+@interface WXActionSheetModule()
+
+@property(nonatomic,copy)NSArray *items;
+@property(nonatomic)BOOL isAnimating;
+@property(nonatomic)NSInteger index;
+
+@end
+
+@implementation WXActionSheetModule
+@synthesize weexInstance;
+WX_PlUGIN_EXPORT_MODULE(actionSheet, WXActionSheetModule)
+WX_EXPORT_METHOD(@selector(create:callback:))
+
+/**
+ create actionsheet
+
+ @param options items
+ @param callback callback
+ */
+-(void)create:(NSDictionary *)options callback:(WXModuleCallback)callback
+{
+    NSArray *items = @[];
+    if (options[@"items"]) {
+        items = options[@"items"];
+    }
+    NSString *title = @"";
+    NSString *message = @"";
+    if (options[@"title"]) {
+        title = [WXConvert NSString:options[@"title"]];
+    }
+    
+    if (options[@"message"]) {
+        message = [WXConvert NSString:options[@"message"]];
+    }
+    
+    UIAlertController* alert = [UIAlertController alertControllerWithTitle:title
+                                                                   message:message
+                                                            preferredStyle:UIAlertControllerStyleActionSheet];
+    for(NSDictionary *dict in items) {
+        NSString *message = @"";
+        NSInteger type = 0;
+        if(dict[@"message"]) {
+            message = [WXConvert NSString:dict[@"message"]];
+        }
+        if(dict[@"type"]) {
+            type = [WXConvert NSInteger:dict[@"type"]];
+        }
+        UIAlertAction *action = [UIAlertAction actionWithTitle:message style:type handler:^(UIAlertAction * _Nonnull action) {
+            for (int i = 0;i < [alert.actions count];i++) {
+                if(action == alert.actions[i]) {
+                    NSDictionary *dict = @{@"index":@(i),@"message":action.title};
+                    callback(@{@"result":@"success",@"data":dict});
+                }
+            }
+        }];
+        [alert addAction:action];
+    }
+    
+    
+    [[[UIApplication sharedApplication] keyWindow] endEditing:YES];  //hide keyboard
+    UIWindow *window = [UIApplication sharedApplication].keyWindow;
+    [window.rootViewController presentViewController:alert animated:YES completion:nil];
+}
+
+@end
